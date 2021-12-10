9885f76b9add9a3a393547de51a356a9c62dd768
torcu <45042365+Exater5@users.noreply.github.com>
2021-12-10 13:36:25 +0100

    Day 10 Completed!

--- Assets/Scripts/Day10.cs
+++ Assets/Scripts/Day10.cs
@@ -9,52 +9,177 @@ public class Day10 : MonoBehaviour
     [SerializeField] string[] rawInput;
     [SerializeField] bool next;
     int currentRaw = 0;
+    [SerializeField] long totalPoints = 0;
+    [SerializeField] List<long> scores;
+ 
+
     void Start()
     {
         rawInput = InputReader.ReadInputInLines(10, 0);
-        Problem1();
+        Problem2();
+    }
+    void Problem2()
+    {
+        char inChar;
+        char outChar;
+        bool finded = false;
+
+        if (currentRaw < rawInput.Length)
+        {
+            for (int i = 0; i < rawInput[currentRaw].Length; i++)
+            {
+                if (rawInput[currentRaw][i].ToString() == "X")
+                {
+                    currentRaw++;
+                    Problem2();
+                    break;
+                }
+                if (outChars.Contains(rawInput[currentRaw][i].ToString()))
+                {
+                    inChar = rawInput[currentRaw][i - 1];
+                    outChar = rawInput[currentRaw][i];
+
+                    if (outChar == ')')
+                    {
+                        outChar = '(';
+                    }
+                    else if (outChar == ']')
+                    {
+                        outChar = '[';
+                    }
+                    else if (outChar == '}')
+                    {
+                        outChar = '{';
+                    }
+                    else
+                    {
+                        outChar = '<';
+                    }
+                    finded = true;
+
+                    if (inChar == outChar)
+                    {
+                        rawInput[currentRaw] = rawInput[currentRaw].Remove(i - 1, 2);
+                        Problem2();
+                    }
+                    else
+                    {
+                        rawInput[currentRaw] = "X";
+                        currentRaw++;
+                        Problem2();
+                    }
+                    break;
+                }
+            }
+            if (!finded)
+            {
+                currentRaw++;
+                Problem2();
+            }
+        }
+        else
+        {
+            FinalChecker();
+        }
+    }
+
+    void FinalChecker()
+    {
+        for (int i = 0; i < rawInput.Length; i++)
+        {
+            totalPoints = 0;
+            for (int j = 0; j < rawInput[i].Length; j++)
+            {
+                char outChar = rawInput[i][rawInput[i].Length - 1 - j];
+                if (outChar != 'X')
+                {
+                    totalPoints *= 5;
+
+                    if (outChar == '(')
+                    {
+                        totalPoints += 1;
+                    }
+                    else if (outChar == '[')
+                    {
+                        totalPoints += 2;
+                    }
+                    else if (outChar == '{')
+                    {
+                        totalPoints += 3;
+                    }
+                    else
+                    {
+                        totalPoints += 4;
+                    }
+                }
+            }
+            if (totalPoints > 0)
+            {
+                scores.Add(totalPoints);
+            }
+        }
+        scores.Sort();
+        print(scores[scores.Count/2]);
     }
 
     void Problem1()
     {
         char inChar;
         char outChar;
-        for (int i = 1; i < rawInput[currentRaw].Length; i++)
+        bool finded = false;
+        int stackedPoints = 0;
+
+        if (currentRaw < rawInput.Length)
         {
-            if (outChars.Contains(rawInput[currentRaw][i].ToString()))
+            for (int i = 0; i < rawInput[currentRaw].Length; i++)
             {
-                inChar = rawInput[currentRaw][i - 1];
-                outChar = rawInput[currentRaw][i];
+                if (outChars.Contains(rawInput[currentRaw][i].ToString()))
+                {
+                    inChar = rawInput[currentRaw][i - 1];
+                    outChar = rawInput[currentRaw][i];
 
-                if (outChar == ')')
-                {
-                    outChar = '(';
+                    if (outChar == ')')
+                    {
+                        outChar = '(';
+                        stackedPoints = 3;
+                    }
+                    else if (outChar == ']')
+                    {
+                        outChar = '[';
+                        stackedPoints = 57;
+                    }
+                    else if (outChar == '}')
+                    {
+                        outChar = '{';
+                        stackedPoints = 1197;
+                    }
+                    else
+                    {
+                        outChar = '<';
+                        stackedPoints = 25137;
+                    }
+                    finded = true;
+                    if (inChar == outChar)
+                    {
+                        rawInput[currentRaw] = rawInput[currentRaw].Remove(i - 1, 2);
+                        Problem1();
+                        break;
+                    }
+                    else
+                    {
+                        totalPoints += stackedPoints;
+                        currentRaw++;
+                        Problem1();
+                        break;
+                    }
                 }
-                else if (outChar == ']')
-                {
-                    outChar = '[';
-                }
-                else if (outChar == '}')
-                {
-                    outChar = '{';
-                }
-                else
-                {
-                    outChar = '<';
-                }
-
-                if (inChar == outChar)
-                {
-                    rawInput[currentRaw] = rawInput[currentRaw].Remove(i - 1, 2);
-                    Problem1();
-                }
-                else
-                {
-                    print(outChar);
-                    currentRaw++;
-                }
-                break;
+            }
+            if (!finded)
+            {
+                currentRaw++;
+                Problem1();
             }
         }
+        
     }
 }
