95df6087e09c6c597492e608a69c7941777b9bb5
torcu <45042365+Exater5@users.noreply.github.com>
2021-12-13 01:31:10 +0100

    Solving 3b

--- Assets/Scripts/Day3.cs
+++ Assets/Scripts/Day3.cs
@@ -67,30 +67,94 @@ public class Day3 : MonoBehaviour
 
     public void Problem2()
     {
-        string[] rawInput = InputReader.ReadInputInLines(3, 0);
-        List<List<int>> convertedNumbers = new List<List<int>>();
+        List<int> mains = new List<int>();
+        List<List<int>> correctIndexes = new List<List<int>>();
+        List<List<int>> columns = new List<List<int>>();
+        string[] rawInput = InputReader.ReadInputInLines(3, 0);      
 
+        for (int i = 0; i < rawInput[0].Length; i++)
+        {
+            columns.Add(new List<int>());
+            correctIndexes.Add(new List<int>());
+        }
         for (int i = 0; i < rawInput.Length; i++)
         {
-            convertedNumbers.Add(new List<int>());
             for (int j = 0; j < rawInput[0].Length; j++)
             {
                 int convertedN = int.Parse(rawInput[i][j].ToString());
-                convertedNumbers[i].Add(convertedN);
+                columns[j].Add(convertedN);
+            }          
+        }
+        for (int i = 0; i < columns.Count; i++)
+        {
+            mains.Add(GetMain(columns[i]));
+        }
+
+        for (int i = 0; i < columns[0].Count; i++)
+        {
+            if (columns[0][i] == mains[0])
+            {
+                correctIndexes[0].Add(i);
             }
         }
 
-        print(convertedNumbers.Count + " " + convertedNumbers[0].Count);
-        //Filas - Columnas
+        for (int i = 1; i < columns.Count; i++)
+        {
+            int currentCount = 0;
+            List<int> validNumbersList = new List<int>();
+            for (int j = 0; j < correctIndexes[i].Count; j++)
+            {
+                validNumbersList.Add(columns[i][correctIndexes[i][j]]);
+            }
+            int nMain = GetMain(validNumbersList);
+            for (int j = 0; j < columns[i].Count; j++)
+            {
+                if (columns[i][j] == nMain && correctIndexes[i - 1].Contains(i))
+                {
+                    currentCount++;
+                    correctIndexes[i].Add(j);
+                }
+            }
+        }
 
+        string result = string.Empty;
 
-        //List<int> zeroAmount = new List<int>();
-        //List<int> oneAmount = new List<int>();
+        for (int i = 0; i < columns.Count; i++)
+        {
+            if(correctIndexes[i].Count > 0)
+            {
+                List<int> validNumbersList = new List<int>();
+                for (int j = 0; j < correctIndexes[i].Count; j++)
+                {
+                    validNumbersList.Add(columns[i][correctIndexes[i][j]]);
+                }
+                result += GetMain(validNumbersList);
+            }
+            else
+            {
+                result += mains[i];
+            }
+        }
 
-        //for (int i = 0; i < convertedNumbers[0].Count; i++)
-        //{
-        //    zeroAmount.Add(0);
-        //    oneAmount.Add(0);
-        //}
+        print(result);
+    }
+
+    public int GetMain(List<int> numbers)
+    {
+        int zeroAmount = 0;
+        int main = 1;
+        for (int i = 0; i < numbers.Count; i++)
+        {
+            if (numbers[i] == 0)
+            {
+                zeroAmount++;
+                if (zeroAmount > numbers.Count/2)
+                {
+                    main = 0;
+                    break;
+                }
+            }
+        }
+        return main;
     }
 }
