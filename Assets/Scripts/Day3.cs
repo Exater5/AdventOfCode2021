1ae08047616e3d6779700c327ae636c70da8c28b
torcu <45042365+Exater5@users.noreply.github.com>
2021-12-13 13:57:01 +0100

    Day 3 solved

--- Assets/Scripts/Day3.cs
+++ Assets/Scripts/Day3.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class Day3 : MonoBehaviour
 {
+    [SerializeField] List<int> checkRaw;
     // Start is called before the first frame update
     void Start()
     {
         Problem2();
+        Problem22();
     }
 
     public void Problem1()
@@ -67,94 +69,161 @@ public class Day3 : MonoBehaviour
 
     public void Problem2()
     {
-        List<int> mains = new List<int>();
-        List<List<int>> correctIndexes = new List<List<int>>();
-        List<List<int>> columns = new List<List<int>>();
+        List<List<int>> raws = new List<List<int>>();
         string[] rawInput = InputReader.ReadInputInLines(3, 0);      
 
-        for (int i = 0; i < rawInput[0].Length; i++)
-        {
-            columns.Add(new List<int>());
-            correctIndexes.Add(new List<int>());
-        }
         for (int i = 0; i < rawInput.Length; i++)
         {
+            raws.Add(new List<int>());
             for (int j = 0; j < rawInput[0].Length; j++)
             {
                 int convertedN = int.Parse(rawInput[i][j].ToString());
-                columns[j].Add(convertedN);
+                raws[i].Add(convertedN);
             }          
         }
-        for (int i = 0; i < columns.Count; i++)
-        {
-            mains.Add(GetMain(columns[i]));
-        }
 
-        for (int i = 0; i < columns[0].Count; i++)
+        for (int i = 1; i < raws[0].Count; i++)
         {
-            if (columns[0][i] == mains[0])
+            int currentMain = GetMain(GetColumnOf(raws, i-1));
+           
+            for (int j = 0; j < raws.Count; j++)
             {
-                correctIndexes[0].Add(i);
-            }
-        }
-
-        for (int i = 1; i < columns.Count; i++)
-        {
-            int currentCount = 0;
-            List<int> validNumbersList = new List<int>();
-            for (int j = 0; j < correctIndexes[i].Count; j++)
-            {
-                validNumbersList.Add(columns[i][correctIndexes[i][j]]);
-            }
-            int nMain = GetMain(validNumbersList);
-            for (int j = 0; j < columns[i].Count; j++)
-            {
-                if (columns[i][j] == nMain && correctIndexes[i - 1].Contains(i))
+                if (GetColumnOf(raws, i-1)[j] != currentMain)
                 {
-                    currentCount++;
-                    correctIndexes[i].Add(j);
+                    raws[j][i] = -1;
                 }
             }
         }
 
-        string result = string.Empty;
+        string sol = string.Empty;
 
-        for (int i = 0; i < columns.Count; i++)
+        for (int i = 0; i < raws[0].Count; i++)
         {
-            if(correctIndexes[i].Count > 0)
-            {
-                List<int> validNumbersList = new List<int>();
-                for (int j = 0; j < correctIndexes[i].Count; j++)
-                {
-                    validNumbersList.Add(columns[i][correctIndexes[i][j]]);
-                }
-                result += GetMain(validNumbersList);
-            }
-            else
-            {
-                result += mains[i];
-            }
+            sol += GetMain(GetColumnOf(raws, i));
         }
-
-        print(result);
+        print(sol);
     }
+
+    public void Problem22()
+    {
+        List<List<int>> raws = new List<List<int>>();
+        string[] rawInput = InputReader.ReadInputInLines(3, 0);
+
+        for (int i = 0; i < rawInput.Length; i++)
+        {
+            raws.Add(new List<int>());
+            for (int j = 0; j < rawInput[0].Length; j++)
+            {
+                int convertedN = int.Parse(rawInput[i][j].ToString());
+                raws[i].Add(convertedN);
+            }
+        }
+
+        for (int i = 1; i < raws[0].Count; i++)
+        {
+            int currentMain = GetNotMain(GetColumnOf(raws, i - 1));
+
+            for (int j = 0; j < raws.Count; j++)
+            {
+                if (GetColumnOf(raws, i - 1)[j] != currentMain)
+                {
+                    raws[j][i] = -1;
+                }
+            }
+        }
+
+        string sol = string.Empty;
+
+        checkRaw = GetColumnOf(raws,2);
+        for (int i = 0; i < raws[0].Count; i++)
+        {
+            sol += GetNotMain(GetColumnOf(raws, i));
+        }
+        print(sol);
+    }
+
+    public List<int> GetColumnOf(List<List<int>> rawList ,int index)
+    {
+        List<int> column = new List<int>();
+
+        for (int i = 0; i < rawList.Count; i++)
+        {
+            column.Add(rawList[i][index]);
+        }
+        return column;
+    }
+
 
     public int GetMain(List<int> numbers)
     {
         int zeroAmount = 0;
+        int oneAmount = 0;
         int main = 1;
         for (int i = 0; i < numbers.Count; i++)
         {
             if (numbers[i] == 0)
             {
                 zeroAmount++;
-                if (zeroAmount > numbers.Count/2)
-                {
-                    main = 0;
-                    break;
-                }
+            }
+            if (numbers[i] == 1)
+            {
+                oneAmount++;
             }
         }
+
+        if (zeroAmount > oneAmount)
+        {
+            main = 0;
+        }
+        if (zeroAmount == 0)
+        {
+            main = 1;
+        }
+        if (oneAmount == 0)
+        {
+            main = 0;
+        }
+        return main;
+    }
+    public int GetNotMain(List<int> numbers)
+    {
+        int zeroAmount = 0;
+        int oneAmount = 0;
+        int main = 0;
+
+        for (int i = 0; i < numbers.Count; i++)
+        {
+            if (numbers[i] == 0)
+            {
+                zeroAmount++;
+            }
+            if (numbers[i] == 1)
+            {
+                oneAmount++;
+            }
+        }
+
+        if (oneAmount > zeroAmount)
+        {
+            main = 0;
+        }
+        else
+        {
+            main = 1;
+            if (oneAmount == zeroAmount)
+            {
+                main = 0;
+            }
+        }
+        if (zeroAmount == 0)
+        {
+            main = 1;
+        }
+        if (oneAmount == 0)
+        {
+            main = 0;
+        }
+
         return main;
     }
 }
